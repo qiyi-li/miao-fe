ff02d45c294eece46ef93baddf82e8d92fe4340d
qiyi-li <errgou@gmail.com>
2022-08-30 11:17:24 +0800

    抽离routes与history

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,13 @@
 import { createApp } from 'vue'
 import {App} from "./App"
-import {createRouter,createWebHashHistory} from 'vue-router'
-import { Bar } from './views/Bar'
-import { Foo } from './views/Foo'
+import {createRouter} from 'vue-router'
+import { routes } from './config/routes'
+import { history } from './shared/history'
 
-const routes = [
-    {path:'/',component:Foo},
-    {path:'/bar',component:Bar}
-]
+
 const router = createRouter({
-    history:createWebHashHistory(),
-    routes
+    history,
+    routes:routes
 })
 
  
