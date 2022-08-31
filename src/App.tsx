7169e15362ad8d8295650db3ff4531f7274c70cb
qiyi-li <errgou@gmail.com>
2022-08-31 17:41:34 +0800

    欢迎页面完成

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,6 @@ import "./App.scss"
 
 export const App = defineComponent({
   setup() {
-    const refCount = ref(0)
-    const onClick = ()=>refCount.value+=1
     return () => (
       <div class="page">
         <RouterView />
