5f1008350d5e8df58ba1e87bfc807bb914818f3c
qiyi-li <errgou@gmail.com>
2022-08-31 17:38:37 +0800

    第一个欢迎界面

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,14 @@
 import { defineComponent, ref } from 'vue'
 import { RouterView } from 'vue-router'
+import "./App.scss"
 
 export const App = defineComponent({
   setup() {
     const refCount = ref(0)
     const onClick = ()=>refCount.value+=1
     return () => (
-      <div>
-        <header>
-        </header>
-        <RouterView/>
-        <footer>footer</footer>
+      <div class="page">
+        <RouterView />
       </div>)
   }
 })
