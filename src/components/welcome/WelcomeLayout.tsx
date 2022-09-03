d4d0655e0a2de86778ab947a3af77e77c6959984
qiyi-li <errgou@gmail.com>
2022-09-03 10:29:55 +0800

    再次重构

--- src/components/welcome/WelcomeLayout.tsx
+++ src/components/welcome/WelcomeLayout.tsx
@@ -1,22 +1,16 @@
-import { defineComponent } from 'vue';
-import s from './First.module.scss';
-import { RouterLink } from 'vue-router';
-export const WelcomeLayout = defineComponent({
-    setup: (prop1s, context) => {
-        const { slots } = context
-        console.log(slots.buttonName?.(),slots.action?.())
-        return () => (
-            <div class={s.wrapper}>
-                <div class={s.card}>
-                    {slots.icon?.()}
-                    {slots.title?.()}
-                </div>
-                <div class={s.actions}>
-                    <RouterLink class={s.fake} to="/start" >跳过</RouterLink>
-                    <RouterLink to={`${slots.action?.()[0]?.children}`} >{slots.buttonName?.()[0]?.children}</RouterLink>
-                    <RouterLink to="/start" >跳过</RouterLink>
-                </div>
-            </div>
-        )
-    }
-})
+import { FunctionalComponent } from 'vue';
+import s from './WelcomeLayout.module.scss';
+export const WelcomeLayout: FunctionalComponent = (props, context) => {
+  const { slots: { icon, title, buttons } } = context
+  return (
+    <div class={s.wrapper}>
+      <div class={s.card}>
+        {icon?.()}
+        {title?.()}
+      </div>
+      <div class={s.actions}>
+        {buttons?.()}
+      </div>
+    </div>
+  )
+}
