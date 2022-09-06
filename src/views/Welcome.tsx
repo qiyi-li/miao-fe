d85af1d04ba75f451c8ed5c8306dd8bb2cc3dd2d
qiyi-li <errgou@gmail.com>
2022-09-06 11:14:53 +0800

    滑动图片切换路由

--- src/views/Welcome.tsx
+++ src/views/Welcome.tsx
@@ -1,22 +1,40 @@
 import { defineComponent, h, ref, Transition, VNode, watchEffect } from 'vue';
-import { RouteLocationNormalizedLoaded, RouterView } from 'vue-router';
+import { RouteLocationNormalizedLoaded, RouterView, useRoute, useRouter } from 'vue-router';
 import s from './Welcome.module.scss'
 import { useSwipe } from '../hooks/useSwipe';
+import { throttle } from '../shared/throttle';
+
+const nameToPathMap: Record<string, string> = {
+  'Welcome1': '/welcome/2',
+  'Welcome2': '/welcome/3',
+  'Welcome3': '/welcome/4',
+  'Welcome4': '/start',
+}
+
 export const Welcome = defineComponent({
   setup: (props, context) => {
-    const main = ref<HTMLElement|null>(null)
-    const {direction,swiping} = useSwipe(main)
+    const main = ref<HTMLElement>()
+    const { direction, swiping, distance } = useSwipe(main, { beforeStart: e => e.preventDefault() })
+    const route = useRoute()
+    const router = useRouter()
+    const push = throttle(() => {
+      const name = (route.name || 'Welcome1').toString()
+      router.push(nameToPathMap[name])
+    }, 500)
     watchEffect(() => {
-      console.log(swiping.value, direction.value)
+      if (swiping.value && direction.value === 'left' && distance.value && distance.value.x < -100) {
+        push()
+      }
     })
+
     return () => <div class={s.wrapper}>
       <header>
-      <svg>
+        <svg>
           <use xlinkHref='#mangosteen'></use>
         </svg>
         <h1>山竹记账</h1>
       </header>
-      <main class={s.main} ref='main'>
+      <main class={s.main} ref={main}>
         <RouterView name="main">
           {({ Component: X, route: R }: { Component: VNode, route: RouteLocationNormalizedLoaded }) =>
             <Transition enterFromClass={s.slide_fade_enter_from} enterActiveClass={s.slide_fade_enter_active}
