4d1a5079fbd927e8d881bf60368fedd71da7715d
qiyi-li <errgou@gmail.com>
2022-09-17 13:45:31 +0800

    add Center

--- src/views/start/Start.tsx
+++ src/views/start/Start.tsx
@@ -1,6 +1,8 @@
 import { defineComponent } from 'vue';
 import { Button } from '../../shared/Button/Button';
+import { Center } from '../../shared/Center/Center';
 import { FloatButton } from '../../shared/FloatButton/FloatButton';
+import { Icon } from '../../shared/Icon/Icon';
 import s from './Start.module.scss'
 export const Start = defineComponent({
     setup(props, context) {
@@ -9,14 +11,15 @@ export const Start = defineComponent({
         }
         return () => (
             <div class={s.start}>
-                Start
                 <nav>menu</nav>
-                <main>
-                    <div class={s.button_wrapper}>
-                        <Button onClick={onClick} class={s.button}>123123123</Button>
-                    </div>
-                    <FloatButton iconName={'add'}/>
-                </main>
+                <Center class={s.pig_wrapper}>
+                    <Icon name="pig" class={s.pig} />
+                </Center>
+
+                <div class={s.button_wrapper}>
+                    <Button onClick={onClick} class={s.button}>123123123</Button>
+                </div>
+                <FloatButton iconName={'add'} />
             </div>
         )
     }
