275c41f6c819f2c4864de624e70f8c10ebed338c
qiyi-li <errgou@gmail.com>
2022-09-20 14:21:32 +0800

    完善数字输入

--- src/components/Item/InputPad/InputPad.tsx
+++ src/components/Item/InputPad/InputPad.tsx
@@ -14,31 +14,75 @@ export const InputPad = defineComponent({
     setup: (props, context) => {
         const refDate = ref<Date>(new Date)
         const show = ref(false)
+        const numsDisplay = ref('0')
+        const lastNum = ref('0')
         const onConfirm = (val: Date) => {
             refDate.value = val
             show.value = false
         }
-        const showDatePicker = ()=>show.value=true
-        const hideDatePicker = ()=>show.value=false
-        
+        const showDatePicker = () => show.value = true
+        const hideDatePicker = () => show.value = false
+        const numsList = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '.']
+        const appendFn = (val: string) => {
+            const value = numsDisplay.value
+            if (value.length === 16) return
+            if (value === '0' && val === '0') return
+            if (value[value.length - 1] === '.' && val === '.') return
+            if (numsList.indexOf(val) < 0) lastNum.value = '' //输入+|-时候充值lastNum
+
+            if (val === '.') {
+                if (numsList.indexOf(value[value.length - 1]) === -1) {//最后一位是+|-
+                    numsDisplay.value += '0' + val
+                    lastNum.value = '0' + val
+                } else if (value === '0') {//value只有0
+                    numsDisplay.value += val
+                    lastNum.value = '0' + val
+                } else if (lastNum.value.indexOf('.') === -1) {//如果最新的数字中没有 '.',再加 '.'
+                    numsDisplay.value += val
+                    lastNum.value += val
+                }
+                return
+            }
+
+            if (numsList.indexOf(val) >= 0 && value === '0') {//输入数字且当前value为'0'
+                numsDisplay.value = val
+                lastNum.value = val
+            } else if (numsList.indexOf(val) === -1 && numsList.indexOf(value[value.length - 1]) === -1) {//如果输入为+|-
+                return
+            } else {
+                numsDisplay.value += val
+                if (numsList.indexOf(val) >= 0) lastNum.value += val
+            }
+            console.log(lastNum.value)
+        }
+        const clear = () => numsDisplay.value = '0'
+        const back = () => {
+            console.log(numsDisplay.value.slice(0, -1))
+            if (numsDisplay.value !== '0' && numsDisplay.value.length > 1) {
+                numsDisplay.value = numsDisplay.value.slice(0, -1)
+            } else if (numsDisplay.value.length === 1) {
+                numsDisplay.value = '0'
+            }
+            lastNum.value = lastNum.value.slice(0, -1)
+        }
         const now = new Date()
         const buttons = [
-            { text: '1', onClick: () => { } },
-            { text: '2', onClick: () => { } },
-            { text: '3', onClick: () => { } },
-            { text: '清空', onClick: () => { } },
-            { text: '4', onClick: () => { } },
-            { text: '5', onClick: () => { } },
-            { text: '6', onClick: () => { } },
-            { text: '+', onClick: () => { } },
-            { text: '7', onClick: () => { } },
-            { text: '8', onClick: () => { } },
-            { text: '9', onClick: () => { } },
-            { text: '-', onClick: () => { } },
-            { text: '.', onClick: () => { } },
-            { text: '0', onClick: () => { } },
-            { text: '删', onClick: () => { } },
-            { text: '提交', onClick: () => { } },
+            { text: () => '1', onClick: () => appendFn('1') },
+            { text: () => '2', onClick: () => appendFn('2') },
+            { text: () => '3', onClick: () => appendFn('3') },
+            { text: () => <span class={s.AC}>AC</span>, onClick: clear }, ,
+            { text: () => '4', onClick: () => appendFn('4') },
+            { text: () => '5', onClick: () => appendFn('5') },
+            { text: () => '6', onClick: () => appendFn('6') },
+            { text: () => '+', onClick: () => appendFn('+') },
+            { text: () => '7', onClick: () => appendFn('7') },
+            { text: () => '8', onClick: () => appendFn('8') },
+            { text: () => '9', onClick: () => appendFn('9') },
+            { text: () => '-', onClick: () => appendFn('-') },
+            { text: () => '.', onClick: () => appendFn('.') },
+            { text: () => '0', onClick: () => appendFn('0') },
+            { text: () => <Icon name={'delete'} class={s.button_icon} />, onClick: back },
+            { text: () => '提交', onClick: () => { } },
         ]
         return () => <>
             <div class={s.dateAndAmount}>
@@ -49,13 +93,13 @@ export const InputPad = defineComponent({
                     <Icon name="date" class={s.icon} />
                     <span>{dayjs(refDate.value).format('YYYY-MM-DD')}</span>
                 </span>
-                <span class={s.amount}>123123</span>
+                <span class={s.amount}>{numsDisplay.value}</span>
             </div>
             <div class={s.buttons}>
                 {buttons.map(button =>
-                    <button onClick={button.onClick}>
-                        <span>
-                            {button.text}
+                    <button onClick={button?.onClick}>
+                        <span class={s.button_container}>
+                            {button?.text?.()}
                         </span>
                     </button>
                 )}
--- src/shared/Icon/Icon.tsx
+++ src/shared/Icon/Icon.tsx
@@ -2,7 +2,7 @@ import { defineComponent, PropType } from 'vue';
 import s from './Icon.module.scss'
 
 export type IconName = 'add' | 'chart' | 'clock' | 'cloud' |
-  'mangosteen' | 'pig' | 'menu' | 'charts' | 'notify' | 'export' | 'left' |'date'|'notes'
+  'mangosteen' | 'pig' | 'menu' | 'charts' | 'notify' | 'export' | 'left' |'date'|'notes'|'delete'
 export const Icon = defineComponent({
     props: {
         name: {
