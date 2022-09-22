6b3c5dbed70ba7c792bc719bb3b08379b54802e2
qiyi-li <errgou@gmail.com>
2022-09-22 16:45:20 +0800

    编辑创建tags页面

--- src/components/Tag/TagCreate/TagCreate.tsx
+++ src/components/Tag/TagCreate/TagCreate.tsx
@@ -1,12 +1,36 @@
-import { defineComponent,PropType } from 'vue';
+import { defineComponent, PropType } from 'vue';
+import { Icon } from '../../../shared/Icon/Icon';
+import { MainLayout } from '../../Layouts/MainLayout';
 import s from './TagCreate.module.scss'
 export const TagCreate = defineComponent({
     props: {
-       name: {
-           type: String as PropType<string>
+        name: {
+            type: String as PropType<string>
         }
     },
-    setup(props,context) {
-        return () => (<div>TagCreate</div>)
+    setup(props, context) {
+        return () => (<MainLayout>{{
+            title: () => '新建标签',
+            icon: () => <Icon name='left' />,
+            main: () => {
+                return <form>
+                    <div class={s.form_item}>
+                        <label for="name">标签名</label>
+                        <input type="text" name="name" />
+                    </div>
+                    <div class={s.form_item}>
+                        <label for="emoji">符号</label>
+                        <input type="text" name="emoji" />
+
+                    </div>
+                    <div>
+                        <p>记账时长按标签即可进行编辑</p>
+                    </div>
+                    <div>
+                        <button>确定</button>
+                    </div>
+                </form>
+            }
+        }}</MainLayout>)
     }
 })
