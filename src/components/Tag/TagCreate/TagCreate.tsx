4a1fb859e513a854a5fce097921836ffd2286c70
qiyi-li <errgou@gmail.com>
2022-09-23 17:57:44 +0800

    添加form校验

--- src/components/Tag/TagCreate/TagCreate.tsx
+++ src/components/Tag/TagCreate/TagCreate.tsx
@@ -1,7 +1,8 @@
-import { defineComponent, PropType, reactive, ref } from 'vue';
+import { defineComponent, PropType, reactive, ref, toRaw } from 'vue';
 import { Button } from '../../../shared/Button/Button';
 import { EmojiSelect } from '../../../shared/EmojiSelect/EmojiSelect';
 import { Icon } from '../../../shared/Icon/Icon';
+import { FData, Rules, validate } from '../../../shared/validate';
 import { MainLayout } from '../../Layouts/MainLayout';
 import s from './TagCreate.module.scss'
 export const TagCreate = defineComponent({
@@ -11,21 +12,30 @@ export const TagCreate = defineComponent({
         }
     },
     setup(props, context) {
-        const selectEmoji = ref('')
-        const formData = reactive({
+        const errors = reactive<{ [f in keyof FData]?: string[] }>({})
+        const formData: FData = reactive({
             name: '',
-            sign: 'x',
+            sign: '',
         })
+        const onSubmit = (e: Event) => {
+            e.stopPropagation()
+            const roles: Rules<typeof formData> = [
+                { key: 'name', type: 'required', value: true, },
+                { key: 'name', type: 'pattern', value: /^.{1,4}$/, message: '只能填 1 到 4 个字符' },
+                { key: 'sign', type: 'required', value: true, }
+            ]
+            Object.assign(errors, validate(roles, formData))
+        }
         return () => (<MainLayout>{{
             title: () => '新建标签',
             icon: () => <Icon name='left' />,
             main: () => {
-                return <form class={s.form}>
+                return <form class={s.form} onSubmit={(e) => onSubmit(e)}>
                     <div class={s.form_item}>
                         <label for="name">标签名</label>
-                        <input type="text" class={s.input} name="name" />
+                        <input type="text" v-model={formData.name} class={s.input} name="name" />
                         <div class={s.form_item_errorHint}>
-                            <span>必填</span>
+                            <span>{errors?.name?.[0]}</span>
                         </div>
                     </div>
                     <div class={[s.form_item,]}>
@@ -34,7 +44,7 @@ export const TagCreate = defineComponent({
                             <EmojiSelect v-model:sign={formData.sign} />
                         </div>
                         <div class={s.form_item_errorHint}>
-                            <span>必填</span>
+                            <span>{errors?.sign?.[0]}</span>
                         </div>
                     </div>
                     <p class={s.tips}>记账时长按标签即可进行编辑</p>
---
+++ src/shared/validate.tsx
@@ -0,0 +1,42 @@
+type FData = {
+    name?: string,
+    sign?: string
+}
+type Rule<T> = {
+    key: keyof T,
+    message?: string
+} & ({ type: 'required', value: boolean } | { type: 'pattern', value: RegExp, message: string })
+
+type Rules<T> = Rule<T>[]
+
+export type { Rules, Rule, FData }
+
+export const validate = <T extends FData>(Rules: Rules<T>, formData: T) => {
+    type Errors = {
+        [k in keyof T]?: string[]
+    }
+    const errors: Errors = {}
+    Rules.forEach((Rule) => {
+        const { type, value, key, message } = Rule
+        
+        const itemValue = formData[key]
+        switch (type) {
+            case "pattern":
+                if (itemValue && !value.test(itemValue.toString())) {
+                    console.log(errors)
+                    errors[key] = errors[key] ?? []
+                    errors[key]?.push(message)
+                }
+                break;
+            case "required":
+                if (value && (!itemValue || itemValue === null)) {
+                    errors[key] = errors[key] ?? []
+                    errors[key]?.push('必填')
+                }
+                break;
+            default:
+                return
+        }
+    })
+    return errors
+}
