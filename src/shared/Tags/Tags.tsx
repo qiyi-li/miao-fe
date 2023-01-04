5e4a254a4d33cbd3d505666ae9f576c22f362996
qiyi.li <errgou@gmail.com>
2023-01-04 17:13:44 +0800

    长安跳转编辑页面

--- src/shared/Tags/Tags.tsx
+++ src/shared/Tags/Tags.tsx
@@ -1,10 +1,10 @@
-import {defineComponent, onUpdated, PropType} from 'vue';
+import {defineComponent, PropType, ref} from 'vue';
 import {useTags} from '../useTags';
 import s from './Tags.module.scss';
 import {http} from '../HttpClient';
 import {Icon} from '../Icon/Icon';
 import {Button} from '../Button/Button';
-import {RouterLink} from 'vue-router';
+import {RouterLink, useRouter} from 'vue-router';
 
 export const Tags = defineComponent({
   props: {
@@ -23,11 +23,30 @@ export const Tags = defineComponent({
         _mock: 'tagIndex'
       });
     });
+    const router = useRouter();
+    const timer = ref<number>();
+    const currentTag = ref<HTMLDivElement>();
+    const onTouchStart = (e: TouchEvent, tag: Tag) => {
+      currentTag.value = e.currentTarget as HTMLDivElement;
+      timer.value = setTimeout(async () => {
+        await router.push(`/tags/${tag.id}/edit?kind=${props.kind}&return_to=${router.currentRoute.value.fullPath}`);
+        //触发长按
+      }, 500);
+    };
+    const onTouchEnd = () => {
+      clearTimeout(timer.value);
+    };
+    const onTouchMove = (e: TouchEvent) => {
+      const pointedElement = document.elementFromPoint(e.touches[0].clientX, e.touches[0].clientY);
+      if (currentTag.value !== pointedElement && currentTag.value?.contains(pointedElement) === false) {
+        clearTimeout(timer.value);
+      }
+    };
     const onSelect = (tag: Tag) => {
       context.emit('update:selected', tag.id);
     };
     return () => <>
-      <div class={s.tags_wrapper}>
+      <div class={s.tags_wrapper} onTouchmove={onTouchMove}>
         <RouterLink to={`/tags/create?kind=${props.kind}`}>
           <div class={s.tag}>
             <div class={s.sign}>
@@ -41,6 +60,8 @@ export const Tags = defineComponent({
         {tags.value.map(tag =>
           <div class={[s.tag, props.selected === tag.id ? s.selected : '']}
                onClick={() => onSelect(tag)}
+               onTouchstart={(e) => onTouchStart(e, tag)}
+               onTouchend={onTouchEnd}
           >
             <div class={s.sign}>
               {tag.sign}
